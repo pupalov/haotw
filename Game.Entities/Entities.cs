aef59058dadebcef30c801bb1e283fa05635b421
Pupal <44545666+pupalov@users.noreply.github.com>
2018-12-17 00:02:29 +0900

    Added Game.Entities folder

---
+++ Game.Entities/Entities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Entities
+{
+    public class MainCharacter
+    {
+        public void HealPoint()
+        {
+            int HP = 5;
+        }
+    }
+
+
+}
